d93d289999fe17b5bc47ded5662f943c70001895
divyank00 <lunkaddivyank@gmail.com>
2020-11-13 01:26:47 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-dcb32387a2f0d6aab796.js"
+    "url": "webpack-runtime-631d83e611b2884c6a52.js"
   },
   {
     "url": "framework-b6dd62d0094bcee1981d.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3bc4df9284db00ddd836436105ec12ee"
+    "revision": "bad18b8684ae806edeca0f56ad7faba9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
